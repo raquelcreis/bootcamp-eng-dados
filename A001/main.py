a6936c7e50f4599984edf2b1014dd2baf8941a21
Raquel Reis <64446494+raquelcreis@users.noreply.github.com>
2023-01-31 13:36:37 -0300

    primeiro commit

--- A001/main.py
+++ A001/main.py
@@ -3,8 +3,12 @@ import pandas as pd
 
 url = 'https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados?modalidade=Lotofácil'
 
-r = requests.get(url)
+r = requests.get(url, verify=False)
 
 r_text = r.text.replace('\\r\\n','')
 r_text = r.text.replace('"\r\n}','')
 r_text = r.text.replace('{\r\n  "html":','')
+df = pd.read_html(r_text)
+df1 = df
+df=df[0].copy()
+
