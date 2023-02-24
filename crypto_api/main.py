dfa9d34c724a2804f2e0a4c9d1f1c3b36f2e0594
Raquel Reis <64446494+raquelcreis@users.noreply.github.com>
2023-02-24 19:05:33 -0300

    refactoring

---
+++ crypto_api/main.py
@@ -0,0 +1,22 @@
+import datetime
+from schedule import repeat, every, run_pending
+import time
+from apis import *
+from ingestors import *
+from writers import *
+
+if __name__ == "__main__":
+
+    day_summary_ingestor = DaySummaryIngestor(
+        writer=DataWriter,
+        coins=["BTC","ETH","LTC","BCH"],
+        default_start_date=datetime.date(2021,6,1))
+
+    @repeat(every(1).seconds)
+    def job():
+        day_summary_ingestor.ingest()
+        
+    while True:
+        run_pending()
+        time.sleep(0.5)
+    
